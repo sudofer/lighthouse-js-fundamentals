b3d453fb4d5840a74778eebc5317f87541c07a34
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-04-12 17:07:19 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,16 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+let raining = true
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
