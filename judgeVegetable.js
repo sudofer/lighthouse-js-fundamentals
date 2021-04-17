034da06f189e595f6cdedda95342c60c404467e4
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-04-17 18:29:38 +0000

    judgeVegetable.js

---
+++ judgeVegetable.js
@@ -0,0 +1,25 @@
+// const vegetables = [
+//   {
+//     submitter: 'Old Man Franklin',
+//     redness: 10,
+//     plumpness: 5
+//   },
+//   {
+//     submitter: 'Sally Tomato-Grower',
+//     redness: 2,
+//     plumpness: 8
+//   },
+//   {
+//     submitter: 'Hamid Hamidson',
+//     redness: 4,
+//     plumpness: 3
+//   }
+// ]
+
+// const metric = 'redness';
+
+const judgeVegetable = (vegetables, metric) => 
+  vegetables.sort((a, b) => b[metric] - a[metric])[0].submitter; 
+
+
+console.log(judgeVegetable(vegetables, metric));
